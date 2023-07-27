9e9c080a8c87e0b0b596586010a8e439757fb9bb
Clay_MM <clay.moyer.1515@gmail.com>
2023-07-26 20:09:52 -0400

    final touches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,40 @@
 let dailyBlock = $("#timeSlotContainer").children();
 let today = dayjs();
-// let currentDate = $("#currentDay")
 
 $(function () {
+
   //Add a listener for click events on the save button.
   var textArea = document.querySelectorAll("textArea");
   $('.saveBtn').on('click', saveEvent);
 
+  //save to local storage
   function saveEvent() {
     let itemDescription = $(this).siblings(".description").val();
     let itemTime = $(this).siblings(".hour").text();
     localStorage.setItem(itemTime, itemDescription);
   }
-  //Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements.
+  //retrieve saved user input from localStorage and set the values of the corresponding textarea elements.
   for (let index = 0; index < textArea.length; index++) {
     textArea[index].value = localStorage.getItem(textArea[index].id);
   }
 
   dailyBlock.each(function() {
     elementHour = Number($(this).attr('id').split('-')[1]);
-    presentHour = Number(today.format('H'));
-    console.log("elementHour:", elementHour);
-    console.log("presentHour:", presentHour);
-}); 
-// TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour.
+    presentHour = Number(dayjs().format('H'));
+
+    //code for testing .addClass functionality any time between 5pm and 9am, otherwise they will all be one color. remember to comment out presentHour above
+    // var customPresentHour = dayjs().set('hour', 11).set('minute', 0).set('second', 0);
+    // presentHour = customPresentHour.hour();
+
+//apply the past, present, or future class to each time block by comparing the id to the current hour.
     if ( presentHour === elementHour ) {
-      $(this).addClass('present')
+      $(this).addClass("present")
     } else if ( presentHour > elementHour ) {
-      $(this).addClass('past');
+      $(this).addClass("past");
     } else if ( presentHour < elementHour ) {
-      $(this).addClass('future');
+      $(this).addClass("future");
     }
+  }); 
 
   $("#currentDay").text(today.format('dddd, MMMM D'))
   });
